44ef66a2f7dd95aa928b28dff5a2dd115b4597ca
Vishva45 <vishvaraja77@gmail.com>
2026-08-30 19:27:08 +0530

    second change

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example"
+  name     = "example2"
   location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
 }
